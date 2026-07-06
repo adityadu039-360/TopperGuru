3679f9177c77ce541e07c5337a98b3dfe34988be
adityadu039-360 <adityadu039@gmail.com>
2026-07-06 19:33:24 +0530

    Connect home page to 11th standard page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'sslc_page.dart';
+import 'class11_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -15,6 +16,7 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            // 10th Button
             ElevatedButton(
               onPressed: () {
                 Navigator.push(
@@ -29,13 +31,22 @@ class HomePage extends StatelessWidget {
 
             const SizedBox(height: 15),
 
+            // 11th Button
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const Class11Page(),
+                  ),
+                );
+              },
               child: const Text('11th'),
             ),
 
             const SizedBox(height: 15),
 
+            // 12th Button
             ElevatedButton(
               onPressed: () {},
               child: const Text('12th'),
@@ -43,6 +54,7 @@ class HomePage extends StatelessWidget {
 
             const SizedBox(height: 15),
 
+            // Topper Corner Button
             ElevatedButton(
               onPressed: () {},
               child: const Text('Topper Corner'),
