97d8669e8a5c86a9b7251437f24ed28a8b957578
adityadu039-360 <adityadu039@gmail.com>
2026-07-11 09:46:53 +0530

    Connect downloads page with local storage

--- lib/downloads_page.dart
+++ lib/downloads_page.dart
@@ -1,7 +1,31 @@
 import 'package:flutter/material.dart';
+import 'download_manager.dart';
 
-class DownloadsPage extends StatelessWidget {
+class DownloadsPage extends StatefulWidget {
   const DownloadsPage({super.key});
+
+  @override
+  State<DownloadsPage> createState() => _DownloadsPageState();
+}
+
+class _DownloadsPageState extends State<DownloadsPage> {
+  List<String> downloads = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadDownloads();
+  }
+
+  Future<void> loadDownloads() async {
+    downloads = await DownloadManager.getDownloads();
+    setState(() {});
+  }
+
+  Future<void> deletePdf(String pdfName) async {
+    await DownloadManager.removePdf(pdfName);
+    loadDownloads();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,37 +34,61 @@ class DownloadsPage extends StatelessWidget {
         title: const Text("Downloads"),
         centerTitle: true,
       ),
-      body: const Center(
+      body: downloads.isEmpty
+          ? const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Icon(
               Icons.download_done,
-              size: 90,
-              color: Colors.blue,
+              size: 80,
+              color: Colors.grey,
             ),
             SizedBox(height: 20),
             Text(
               "No Downloads Yet",
               style: TextStyle(
-                fontSize: 24,
+                fontSize: 22,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 10),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30),
-              child: Text(
-                "Downloaded PDFs will appear here automatically.",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey,
-                ),
-              ),
+            SizedBox(height: 8),
+            Text(
+              "Downloaded PDFs will appear here",
+              style: TextStyle(color: Colors.grey),
             ),
           ],
         ),
+      )
+          : ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemCount: downloads.length,
+        itemBuilder: (context, index) {
+          return Card(
+            elevation: 5,
+            margin: const EdgeInsets.only(bottom: 12),
+            child: ListTile(
+              leading: const Icon(
+                Icons.picture_as_pdf,
+                color: Colors.red,
+                size: 34,
+              ),
+              title: Text(
+                downloads[index],
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: const Text("Saved on this device"),
+              trailing: IconButton(
+                icon: const Icon(Icons.delete),
+                onPressed: () {
+                  deletePdf(downloads[index]);
+                },
+              ),
+            ),
+          );
+        },
       ),
     );
   }
